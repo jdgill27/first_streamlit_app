a5b7484a6485696e1c4edbfc5e2f0ea0bed12bc7
jdgill27 <62246495+jdgill27@users.noreply.github.com>
2023-01-09 10:43:54 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.write(':oatmeal: Omega 3 & Blueberry Oatmeal')
+streamlit.write(':kale: Omega 3 & Blueberry Oatmeal')
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-range Egg")
 
